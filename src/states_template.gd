63bd91d1102fa362d0577116b0143c2a800034c5
azylazide <60816921+azylazide@users.noreply.github.com>
2023-07-12 23:38:23 +0800

    fix broken state name dict

--- src/states_template.gd
+++ src/states_template.gd
@@ -22,14 +22,13 @@ func _init(current_state) -> void:
 	current = current_state
 
 func _name_dict(enum_dic) -> void:
-	print(state_name)
-	state_name = enum_dic.keys().map(func(elem):return elem.to_pascal_case())
-	
-	
-#	state_name[-2] = "NULL"
-#	state_name[-1] = "AUTO"
-	print(enum_dic)
-	print(state_name)
+	var temp_name = enum_dic.keys().map(func(elem):return elem.to_pascal_case())
+	state_name[-2] = "Null"
+	state_name[-1] = "Auto"
+	var i:= 0
+	for state in temp_name:
+		state_name[i] = state
+		i+=1
 	
 func change_state() -> void:
 	self.previous_frame = self.current
