6e95ee2313acfe9dc7904df55c002e995abe4c84
Azyl <superj1e2z6@gmail.com>
2023-08-10 22:28:19 +0800

    added temp attack ability flags for player

--- src/actors/player/player_resource.gd
+++ src/actors/player/player_resource.gd
@@ -2,6 +2,7 @@ extends ActorResource
 class_name PlayerResource
 
 @export_flags("DASH","AJUMP","WALL") var abilities:= 0b111
+@export_flags("NORMAL","CHARGED") var shot_attacks:= 0b11
 
 @export var hurt_time:= 0.8
 #attacks and inventory
