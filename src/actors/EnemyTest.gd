373cf77d3e9a00c80b8b94d33958fe88d4b60372
Azyl <superj1e2z6@gmail.com>
2023-08-19 21:46:32 +0800

    fixed test enemy not flipping

--- src/actors/EnemyTest.gd
+++ src/actors/EnemyTest.gd
@@ -7,12 +7,12 @@ func _physics_process(delta: float) -> void:
 	velocity.x = max_run_tile*Globals.TILE_UNITS*direction
 	velocity.y = 10*Globals.TILE_UNITS
 	move_and_slide()
-	
+
+
 	if $Node2D/ShapeCast2D.is_colliding() and direction > 0:
-		direction = -$Node2D/ShapeCast2D.get_collision_normal(0).x
+		direction = $Node2D/ShapeCast2D.get_collision_normal(0).x
 	elif $Node2D/ShapeCast2D2.is_colliding() and direction < 0:
-		direction = -$Node2D/ShapeCast2D2.get_collision_normal(0).x
-	
+		direction = $Node2D/ShapeCast2D2.get_collision_normal(0).x
 	if direction == 0:
 		direction = 1
 	pass
